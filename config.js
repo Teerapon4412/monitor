7e36750a6895d0dc27ed7975257a9df309a8390a
CodexSandboxOffline <CodexSandboxOffline@CO24-00044.hitachichemical.com>
2026-04-08 18:43:57 +0700

    Configure Supabase publishable credentials

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.monitorConfig = {
   supabase: {
-    url: "",
-    anonKey: ""
+    url: "https://vujehsuvfusiejymmcuz.supabase.co",
+    anonKey: "sb_publishable_9j1IS6USb4zSISg1m-BqTA_YE4MpDMC"
   }
 };
